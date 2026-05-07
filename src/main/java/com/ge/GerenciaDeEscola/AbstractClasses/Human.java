b1b55f306ff1f28caeddc043da1fbcbe3e148f5f
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-05-06 21:41:02 -0300

    Feat: Have  created a abstract class for human

---
+++ src/main/java/com/ge/GerenciaDeEscola/AbstractClasses/Human.java
@@ -0,0 +1,11 @@
+package com.ge.GerenciaDeEscola.AbstractClasses;
+
+public abstract class Human {
+    private String name;
+    private String cpf;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String dateOfBirth;
+    private String gender;
+}
