13d06b2c1ffa4eba1f1d2cf11f3ec740b1b23dbc
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-05-19 22:27:17 -0300

    Feat:Put caracteristcs in the class

--- src/main/java/com/ge/GerenciaDeEscola/Eployers/EployeModel.java
+++ src/main/java/com/ge/GerenciaDeEscola/Eployers/EployeModel.java
@@ -1,6 +1,32 @@
 package com.ge.GerenciaDeEscola.Eployers;
 
-public class EployeModel {
+import com.ge.GerenciaDeEscola.AbstractClasses.Human;
+
+public class EployeModel extends Human{
+    private String office;
+    private double salary;
+
+    public EployeModel(String name, int age, String office, double salary) {
+        super(name, age);
+        this.office = office;
+        this.salary = salary;
+    }
+
+    public String getOffice() {
+        return office;
+    }
+
+    public void setOffice(String office) {
+        this.office = office;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 
     
 }
