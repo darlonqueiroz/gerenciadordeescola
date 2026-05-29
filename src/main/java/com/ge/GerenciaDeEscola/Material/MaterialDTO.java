9e2c65f66a4feafcca34dc37a63d60863d38ca77
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-05-28 23:47:24 -0300

    feat: created atriubute of the materials

--- src/main/java/com/ge/GerenciaDeEscola/Material/MaterialDTO.java
+++ src/main/java/com/ge/GerenciaDeEscola/Material/MaterialDTO.java
@@ -1,4 +1,41 @@
 package com.ge.GerenciaDeEscola.Material;
 
 public class MaterialDTO {
+
+    private String name;
+    private String description;
+    private int quantity;
+
+    public MaterialDTO() {
+    }
+
+    public MaterialDTO(String name, String description, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
