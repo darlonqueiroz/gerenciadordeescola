9bea9eb7ee1062165fb4af1c008b061f17d8eda5
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-05-28 23:42:48 -0300

    Feat: created a DTO

--- src/main/java/com/ge/GerenciaDeEscola/Eployers/EmployDTO.java
+++ src/main/java/com/ge/GerenciaDeEscola/Eployers/EmployDTO.java
@@ -2,5 +2,49 @@ package com.ge.GerenciaDeEscola.Eployers;
 
 public class EmployDTO {
 
+    private String name;
+    private String cpf;
+    private String email;
+    private String phone;
+
+    public EmployDTO(String name, String cpf, String email, String phone) {
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
 
 }
