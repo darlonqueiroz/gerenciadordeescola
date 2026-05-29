193c8b521714c6d1531908d57bbc3e700187d0a7
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-05-28 23:45:54 -0300

    feat:created atributes of the mapper

--- src/main/java/com/ge/GerenciaDeEscola/Eployers/EmployMapper.java
+++ src/main/java/com/ge/GerenciaDeEscola/Eployers/EmployMapper.java
@@ -3,4 +3,14 @@ package com.ge.GerenciaDeEscola.Eployers;
 import com.ge.GerenciaDeEscola.AbstractClasses.Human;
 
 public class EmployMapper extends Human {
+    public static EmployModel toEmployModel(EmployDTO dto) {
+        EmployModel model = new EmployModel();
+        model.setId(dto.getId());
+        model.setName(dto.getName());
+        model.setCpf(dto.getCpf());
+        model.setEmail(dto.getEmail());
+        model.setPhone(dto.getPhone());
+        model.setPosition(dto.getPosition());
+        return model;
+    }
 }
