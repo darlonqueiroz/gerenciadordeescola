a046b76c5334125169bb54801cc9fdc3dfaac92f
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-05-30 19:20:41 -0300

    Feat: created attributes in StudantDTO

--- src/main/java/com/ge/GerenciaDeEscola/Student/StudantDTO.java
+++ src/main/java/com/ge/GerenciaDeEscola/Student/StudantDTO.java
@@ -1,4 +1,41 @@
 package com.ge.GerenciaDeEscola.Student;
 
 public class StudantDTO {
+    private String name;
+    private String email;
+    private String phone;
+
+    public StudantDTO() {
+    }
+
+    public StudantDTO(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
 
