3ecd723210a1c1d482c32a771410461bd8fdb896
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-05-30 19:22:01 -0300

    Feat:created studantMapper

--- src/main/java/com/ge/GerenciaDeEscola/Student/StudantMapper.java
+++ src/main/java/com/ge/GerenciaDeEscola/Student/StudantMapper.java
@@ -1,4 +1,21 @@
 package com.ge.GerenciaDeEscola.Student;
 
 public class StudantMapper {
+    public static StudentModel toModel(Student student) {
+        StudentModel model = new StudentModel();
+        model.setId(student.getId());
+        model.setName(student.getName());
+        model.setEmail(student.getEmail());
+        model.setPhone(student.getPhone());
+        return model;
+    }
+
+    public static Student toEntity(StudentModel model) {
+        Student student = new Student();
+        student.setId(model.getId());
+        student.setName(model.getName());
+        student.setEmail(model.getEmail());
+        student.setPhone(model.getPhone());
+        return student;
+    }
 }
