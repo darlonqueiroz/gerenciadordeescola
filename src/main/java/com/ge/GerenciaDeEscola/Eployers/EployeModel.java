b520f7b35fd387eb05e71b4f1476e2b477e61124
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-06-08 21:59:22 -0300

    FIX: TRY ADJUST SUPR CLASS IN CLASS

--- src/main/java/com/ge/GerenciaDeEscola/Eployers/EployeModel.java
+++ src/main/java/com/ge/GerenciaDeEscola/Eployers/EployeModel.java
@@ -3,11 +3,13 @@ package com.ge.GerenciaDeEscola.Eployers;
 import com.ge.GerenciaDeEscola.AbstractClasses.Human;
 
 public class EployeModel extends Human{
+    private Long id;
     private String office;
     private double salary;
 
-    public EployeModel(String name, int age, String office, double salary) {
-        super(name, age);
+    public EployeModel(Long id, String office, double salary) {
+        super(name, cpf, email, phoneNumber, address, dateOfBirth, gender);
+        this.id = id;
         this.office = office;
         this.salary = salary;
     }
