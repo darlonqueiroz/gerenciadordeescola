16d605e5b98cbdd6e68af4eca2956f38759a400e
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-06-08 22:28:19 -0300

    Feat: created GETTERS AND SETTERS EM CLASS HUMAN

--- src/main/java/com/ge/GerenciaDeEscola/AbstractClasses/Human.java
+++ src/main/java/com/ge/GerenciaDeEscola/AbstractClasses/Human.java
@@ -8,4 +8,66 @@ public abstract class Human {
     private String address;
     private String dateOfBirth;
     private String gender;
+
+    public Human (){
+
+
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
 }
