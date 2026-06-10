ca00630d1d882451960cbc10d088c5c314eaa719
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-06-10 00:06:12 -0300

    Feat: Created student controllers

--- src/main/java/com/ge/GerenciaDeEscola/Student/StudentController.java
+++ src/main/java/com/ge/GerenciaDeEscola/Student/StudentController.java
@@ -1,4 +1,17 @@
 package com.ge.GerenciaDeEscola.Student;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping(name = "/student")
 public class StudentController {
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
 }
