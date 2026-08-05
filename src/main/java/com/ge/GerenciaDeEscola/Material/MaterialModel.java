0dfde82645d8ba4dd5a4af088c285849feb7e4b9
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-08-05 20:00:50 -0300

    Feat: Created attribute list for user to create material yourself

--- src/main/java/com/ge/GerenciaDeEscola/Material/MaterialModel.java
+++ src/main/java/com/ge/GerenciaDeEscola/Material/MaterialModel.java
@@ -1,5 +1,13 @@
 package com.ge.GerenciaDeEscola.Material;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.List;
+
+@Entity
+@Table (name = "material")
 public class MaterialModel {
+    List<MaterialModel>MaterialList;
 
 }
