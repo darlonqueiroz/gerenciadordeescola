af52fc8f73cf56df76264be471936965718c53f7
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-08-05 19:51:39 -0300

    Feat:Created notations correct

--- src/main/java/com/ge/GerenciaDeEscola/ReportCard/ReportCardModel.java
+++ src/main/java/com/ge/GerenciaDeEscola/ReportCard/ReportCardModel.java
@@ -1,4 +1,14 @@
 package com.ge.GerenciaDeEscola.ReportCard;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "report_card")
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReportCardModel {
+
 }
