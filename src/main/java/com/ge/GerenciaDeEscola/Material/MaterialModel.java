0ecb7da3b8ade08946e8644773c2c6229246ea8d
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-08-06 17:23:37 -0300

    Fix: adjust class then not run

--- src/main/java/com/ge/GerenciaDeEscola/Material/MaterialModel.java
+++ src/main/java/com/ge/GerenciaDeEscola/Material/MaterialModel.java
@@ -1,13 +1,22 @@
 package com.ge.GerenciaDeEscola.Material;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.util.List;
 
 @Entity
 @Table (name = "material")
 public class MaterialModel {
-    List<MaterialModel>MaterialList;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
 
 }
