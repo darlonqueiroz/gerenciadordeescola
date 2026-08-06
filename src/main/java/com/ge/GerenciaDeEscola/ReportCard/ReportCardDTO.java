57944166aae5f00e7107cb4f167ac6ad99138729
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-08-06 17:23:00 -0300

    Fix: adjust class then not run

--- src/main/java/com/ge/GerenciaDeEscola/ReportCard/ReportCardDTO.java
+++ src/main/java/com/ge/GerenciaDeEscola/ReportCard/ReportCardDTO.java
@@ -1,29 +1,5 @@
 package com.ge.GerenciaDeEscola.ReportCard;
 
 public class ReportCardDTO {
-    private String studentName;
-    private String grade;
 
-    public ReportCardDTO(String studentName, String grade) {
-        this.studentName = studentName;
-        this.grade = grade;
-    }
-
-    public String getStudentName() {
-
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-
-        this.studentName = studentName;
-    }
-
-    public String getGrade() {
-
-        return grade;
-    }
-
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
+}
--- src/main/java/com/ge/GerenciaDeEscola/ReportCard/ReportCardModel.java
+++ src/main/java/com/ge/GerenciaDeEscola/ReportCard/ReportCardModel.java
@@ -1,14 +1,14 @@
 package com.ge.GerenciaDeEscola.ReportCard;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "report_card")
-@AllArgsConstructor
-@NoArgsConstructor
 public class ReportCardModel {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
 
 }
