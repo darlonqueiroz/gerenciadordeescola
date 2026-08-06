3c4ea5ec8eb4a3e8c5154bdb3121e335f6613578
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-08-06 17:51:38 -0300

    Feat: created screen of login

---
+++ src/main/java/Frontend/Desktop/Main.java
@@ -0,0 +1,9 @@
+package Frontend.Desktop;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ScreenLogin screenLogin = new ScreenLogin();
+
+    }
+}
---
+++ src/main/java/Frontend/Desktop/ScreenLogin.java
@@ -0,0 +1,14 @@
+package Frontend.Desktop;
+
+import javax.swing.*;
+
+public class ScreenLogin  {
+    JPanel panel = new JPanel();
+    public ScreenLogin(){
+
+    panel.setSize(400,800);
+    panel.setVisible(true);
+
+
+    }
+}
