e798203dc98dd243470f22a3ba9eb8c9fb3d6f24
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-08-12 22:18:21 -0300

    Feat: created function save

--- src/main/java/com/ge/GerenciaDeEscola/Material/MaterialService.java
+++ src/main/java/com/ge/GerenciaDeEscola/Material/MaterialService.java
@@ -1,6 +1,10 @@
 package com.ge.GerenciaDeEscola.Material;
 
+import graphql.ExecutionResult;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MaterialService {
@@ -12,4 +16,23 @@ public class MaterialService {
     }
 
 
+    public MaterialModel saveMaterial(MaterialModel materialModel){
+        return materialRepository.save(materialModel);
+    }
+
+    public MaterialModel getMaterialById(Long id){
+        return materialRepository.findById(id).orElse(null);
+    }
+
+    public void deleteMaterial(Long id){
+        materialRepository.deleteById(id);
+    }
+
+
+    public List<MaterialModel> createMaterial(@Valid MaterialModel material) {
+        MaterialModel savedMaterial = materialRepository.save(material);
+        return materialRepository.findAll();
+
+
+    }
 }
