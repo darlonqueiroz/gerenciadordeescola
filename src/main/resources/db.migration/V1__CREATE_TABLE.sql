aeaefc05d0b55c44b6202aac81095470cecbc826
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-08-12 22:26:39 -0300

    Refactor: was modificated for add flyway

---
+++ src/main/resources/db.migration/V1__CREATE_TABLE.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS tabela_teste_inicial (
+                                                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                                    nome VARCHAR(255)
+    );
